355c133f733f72835c9323c9af6c8c0c2c0290e1
sunnysingharapidinno <sunnysingha911@gmail.com>
2026-02-18 05:42:28 +0530

    added github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Visual-Condition-Builder/",
   plugins: [
     react({
       babel: {
